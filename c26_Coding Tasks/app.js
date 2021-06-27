473c3d451c8e3c6a9ec4a515f4c07ee96e7d5b4f
Aqeela66 <83452367+Aqeela66@users.noreply.github.com>
2021-06-27 23:41:11 +0500

    Class Tasks C26

---
+++ c26_Coding Tasks/app.js
@@ -0,0 +1,18 @@
+let students = [{
+        name: "Amna",
+        hobbies: ["eating", "cooking"]
+    },
+    {
+        name: "Daniyal",
+        hobbies: ["arts", "shopping"]
+    },
+    {
+        name: "Fahad",
+        hobbies: ["coding", "cooking"]
+    },
+    {
+        name: "Hajra",
+        hobbies: ["sleep", "reading"]
+    }
+];
+console.log(students);
--- calculator/app.js
+++ calculator/app.js
@@ -23,6 +23,6 @@ function divOp(){
     var a,b,c;
     a=Number(document.getElementById("first").value);
     b=Number(document.getElementById("second").value);
-    c=a % b;
+    c=a / b;
     document.getElementById("answer").value= c;
 }
